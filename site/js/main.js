a4be0f30c918cba1b0f2d0774b9222f8356ccc5b
«skai»sstsdev <skaissts.dev@gmail.com>
2026-06-05 13:28:08 +0200

    feat(scroll): make Lenis smooth scroll more buttery

--- site/js/main.js
+++ site/js/main.js
@@ -526,9 +526,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let lenis;
   if (typeof Lenis !== 'undefined') {
     lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.6,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
+      wheelMultiplier: 1.1,
       touchMultiplier: 2
     });
     function raf(time) {
